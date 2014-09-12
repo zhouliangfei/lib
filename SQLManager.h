8303df0f696510f9ba78c054904ad5e4f4bd905a
zhouliangfei <383541328@qq.com>
2014-09-12 15:01:26 +0800

    Create SQLManager.h

---
+++ SQLManager.h
@@ -0,0 +1,43 @@
+//
+//  SQLManager.h
+//  dataTest
+//
+//  Created by mac on 13-6-17.
+//  Copyright (c) 2013年 383541328@qq.com All rights reserved.
+//
+#import <Foundation/Foundation.h>
+//
+#ifndef SQLManager_h
+#define SQLManager_h
+    #define SQLITE_VOID       1978
+#endif
+
+//********************************************
+@interface SQLTransaction : NSObject
+-(BOOL)end;
+-(BOOL)begin;
+-(BOOL)commit;
+-(BOOL)rollback;
+@end
+
+//********************************************
+@interface SQLManager : NSObject
++(SQLManager*)shareInstance;
+//
+@property(nonatomic,readonly) SQLTransaction *transaction;
+-(BOOL)connect:(NSString *)path;
+-(void)close;
+//
+-(BOOL)query:(NSString *)sql;
+-(id)fetch:(NSString *)sql;
+@end
+
+//********************************************
+@interface SQLObject : NSObject
++(BOOL)mapping:(NSString*)primaryKey;
++(id)find:(NSString *)sql;
+//
+-(id)initWithDictionary:(NSDictionary*)data;
+-(BOOL)delete;
+-(BOOL)save;
+@end
