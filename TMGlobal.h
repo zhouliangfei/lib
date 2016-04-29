647a37d8fc742dd8e9215e45e2f4f65acf1aff29
zhouliangfei <383541328@qq.com>
2016-04-29 10:05:08 +0800

    Update TMGlobal.h

--- TMGlobal.h
+++ TMGlobal.h
@@ -12,16 +12,3 @@
 +(void)setValue:(id)value forKey:(NSString*)forKey;
 +(id)valueForKey:(NSString*)forKey;
 @end
-
-//
-id filterEmpty(id object);
-NSString *uuid();
-NSString *md5(NSString *string);
-NSString *base64Encoded(NSString *string);
-NSString *base64Decoded(NSString *string);
-
-//
-NSString *pathForTemporary(NSString *path);
-NSString *pathForDocument(NSString *path);
-NSString *pathForResource(NSString *path);
-NSString *pathForLibrary(NSString *path);
